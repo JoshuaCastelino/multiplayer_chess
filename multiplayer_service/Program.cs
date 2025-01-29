f1531056e652c5449e4f686d4d34456a82dd8d3e
joshua.castelino <joshua.castelino@bp.com>
2025-01-29 09:15:05 +0000

    Added axios api call + removed dirs that should have been git ignores

--- multiplayer_service/Program.cs
+++ multiplayer_service/Program.cs
@@ -4,6 +4,15 @@ using System.Collections.Generic;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.SetIsOriginAllowed(origin => new Uri(origin).Host == "localhost") // Allow any localhost port
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -13,15 +22,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-Dictionary<string, int> lobbyCodesToGameState = new Dictionary<string, string>();
+Dictionary<string, string> lobbyCodesToGameState = new Dictionary<string, string>();
 
 app.UseHttpsRedirection();
 
 app.MapGet("/test", () => "Hello, world!");
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
