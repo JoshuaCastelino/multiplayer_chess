9cba903ee3596c034ff39cc4040c36d736f01b20
JoshuaCastelino <67103454+JoshuaCastelino@users.noreply.github.com>
2025-02-07 20:23:20 +0000

    testing urls

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,6 +1,6 @@
 import { HubConnectionBuilder, HubConnectionState } from "@microsoft/signalr";
 export let connection = new HubConnectionBuilder()
-  .withUrl("https://13.61.19.108:443/gamehub")
+  .withUrl("http://13.61.19.108:443/gamehub")
   .withAutomaticReconnect()
   .build();
 
