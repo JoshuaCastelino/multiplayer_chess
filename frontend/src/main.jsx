2f348ca5488f06d47b4d9700b92228716ceb4aeb
JoshuaCastelino <67103454+JoshuaCastelino@users.noreply.github.com>
2025-03-16 20:25:29 +0000

    use env variable

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,15 +3,13 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import RouteDefinitions from "./RouteDefinitions.jsx";
 import "./index.css";
-import Login from "./Login.jsx";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       {
-        <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
-          >
+        <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
           <RouteDefinitions />
         </GoogleOAuthProvider>
       }
